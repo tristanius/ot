84c3261ee117cdebcd38a91319d1c3b203322b8f
Yeison <yeisontorrado@termotecnica.com.co>
2017-07-31 09:47:55 -0500

    corrección de dirección de validación POST

--- application/views/reportes/edit/edit.php
+++ application/views/reportes/edit/edit.php
@@ -133,7 +133,7 @@
       <img src="<?= base_url('assets/img/ajax-loader2.gif') ?>" ng-show="spinner" alt="">
       <div class="">
         <button id="guardar_reporte" type="button" class="light-green lighten-5 waves-effect waves-light btn mini-btn2" ng-if="validPriv(38)"
-            ng-click="validarRecursos('<?= site_url('reporte/validarRecursos/post/TRUE') ?>')">
+            ng-click="validarRecursos('<?= site_url('reporte/validarRecursos/post') ?>')">
           <b data-icon="&#xe015;"></b> Validar
         </button>
       </div>
