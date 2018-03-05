6f8a2d7d7cfc11066c0e1af4d1ee9aed99b7d773
Yeison Torrado <yeiman_4@hotmail.com>
2018-03-05 16:45:51 -0500

    Ahi vamos de nuevo

--- application/views/miscelanios/consolidado/listado_consolidado.php
+++ application/views/miscelanios/consolidado/listado_consolidado.php
@@ -4,15 +4,16 @@
   header('Cache-Control: max-age=0');
 ?>
 <table id="info_rd_condensado" class="mytabla" >
+
+<caption style="border:1px solid #333" colspan="9">
+  Consolidado de frentes y actividades generado en: <?= $data->fecha ?>
+</caption>
+
 <?php foreach ($reportes->result() as $key => $rd): ?>
 
   <?php if ( isset($rd->condensado) ): ?>
 
       <?php $data = json_decode($rd->condensado) ?>
-
-      <caption style="border:1px solid #333">
-        Consolidado de frentes y actividades generado en: <?= $data->fecha ?>
-      </caption>
       <thead>
         <tr style="background: #6FA5ED">
           <th>Orden</th>
@@ -27,35 +28,39 @@
         </tr>
       </thead>
 
-      <?php foreach ($data->frentes as $key => $frente): ?>
-      <tbody>
-        <tr>
-          <td colspan="9"> <b> <?=  $frente->nombre+' '+$frente->ubicacion ?> </b>  </td>
-        </tr>
+      <?php if (isset($data->frentes)): ?>
 
-        <?php foreach ($frente->items as $key => $it): ?>
-        <tr>
-          <td><?= $it->nombre_ot ?></td>
-          <td><?= $it->nombre_frente ?></td>
-          <td><?= $it->fecha_reporte ?></td>
-          <td><?= $it->itemc_item ?></td>
-          <td class="tableexport-string"> <span> <?= $it->descripcion ?> </span> </td>
-          <td><?= $it->unidad ?></td>
-          <td><?= $it->item_asociado.' ('.$it->descripcion_asociada.') '; ?></td>
-          <td><?= $it->total ?></td>
-          <td>
-            <?php if (!$data->guardado): ?>
-              <span> <?= $it->cantidad_asociada ?> </span>
-            <?php endif; ?>
-            <?php if ($it->alerta): ?>
-              <span style="color:red">La cantidad ingresada supera los valores maximos reportados del item en este frente.</span>
-            <?php endif; ?>
-          </td>
-        </tr>
+        <?php foreach ($data->frentes as $key => $frente): ?>
+        <tbody>
+          <tr>
+            <td colspan="9"> <b> <?=  $frente->nombre+' '+$frente->ubicacion ?> </b>  </td>
+          </tr>
+
+          <?php foreach ($frente->items as $key => $it): ?>
+          <tr>
+            <td><?= $it->nombre_ot ?></td>
+            <td><?= $it->nombre_frente ?></td>
+            <td><?= $it->fecha_reporte ?></td>
+            <td><?= $it->itemc_item ?></td>
+            <td class="tableexport-string"> <span> <?= $it->descripcion ?> </span> </td>
+            <td><?= $it->unidad ?></td>
+            <td><?= $it->item_asociado.' ('.$it->descripcion_asociada.') '; ?></td>
+            <td><?= $it->total ?></td>
+            <td>
+              <?php if (!$data->guardado): ?>
+                <span> <?= $it->cantidad_asociada*1 ?> </span>
+              <?php endif; ?>
+              <?php if ($it->alerta): ?>
+                <span style="color:red">La cantidad ingresada supera los valores maximos reportados del item en este frente.</span>
+              <?php endif; ?>
+            </td>
+          </tr>
+          <?php endforeach; ?>
+
+        </tbody>
         <?php endforeach; ?>
 
-      </tbody>
-      <?php endforeach; ?>
+      <?php endif; ?>
 
   <?php endif; ?>
 
