cb2c88d116ebf3767f869bd5d341b953d6bab3dd
Yeison Torrado <yeiman_4@hotmail.com>
2018-06-13 16:41:34 -0500

    Factura: correciones menores 2

--- application/views/factura/factura/form.php
+++ application/views/factura/factura/form.php
@@ -30,16 +30,16 @@
         <div class="padding1ex col s12 m6 l3 end">
           <b>Estado del acta: </b>
 
-          <div class="fixed-action-btn">
-            <a class="btn-floating btn-large red">
-              <i class="large material-icons">mode_edit</i>
-            </a>
-            <ul>
-              <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
-              <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
-              <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
-              <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
-            </ul>
+          <div style="position:relative">
+            <div class="fixed-action-btn">
+              <a class="btn-floating btn-large red">Estado</a>
+              <ul>
+                <li><a class="btn-floating red"><i class="material-icons">insert_chart</i></a></li>
+                <li><a class="btn-floating yellow darken-1"><i class="material-icons">format_quote</i></a></li>
+                <li><a class="btn-floating green"><i class="material-icons">publish</i></a></li>
+                <li><a class="btn-floating blue"><i class="material-icons">attach_file</i></a></li>
+              </ul>
+            </div>
           </div>
 
           <select class="" ng-model="factura.estado_acta" ng-init="factura.estado_acta = (factura.estado_acta?factura.estado_acta:'EN CREACION')">
@@ -126,6 +126,9 @@
 
 <script type="text/javascript">
 $(document).ready(function(){
-  $('.fixed-action-btn').floatingActionButton();
+  $('.fixed-action-btn').floatingActionButton({
+    direction: 'left',
+    hoverEnabled: false
+  });
 })
 </script>
