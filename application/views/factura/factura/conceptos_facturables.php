b1ecc66fa800732f822764cd0b0384724c39db78
Yeison Torrado <yeiman_4@hotmail.com>
2018-06-21 10:46:55 -0500

    Adiciones para conceptos facturables adicionales 4

--- application/views/factura/factura/conceptos_facturables.php
+++ application/views/factura/factura/conceptos_facturables.php
@@ -5,21 +5,22 @@
   <button type="button" class="btn light-blue accent-4 modal-trigger" href="#add_conceptos_facturables"> <i data-icon="i"></i> </button>
   <br>
   <?php $this->load->view('factura/factura/modales/add_conceptos_facturables'); ?>
-  <table class="mytabla">
+  <table class="mytabla font10">
     <thead>
-      <tr>
+      <tr style="background: #EEE">
         <th>No.</th>
         <th>Item</th>
         <th>Concepto</th>
         <th>Tipo</th>
         <th>Valor</th>
       </tr>
-      <tr>
+      <tr style="background: #EEE">
         <th></th>
         <th> <input type="text" placeholder="Filtro item" ng-model="filtroConceptoFactura.item"> </th>
         <th> <input type="text" placeholder="Filtro concepto" ng-model="filtroConceptoFactura.concepto"> </th>
         <th> <input type="text" placeholder="Filtro tipo" ng-model="filtroConceptoFactura.tipo"> </th>
-        <th> </th>
+        <th> <input type="text" placeholder="Filtro tipo" ng-model="filtroConceptoFactura.valor"> </th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
