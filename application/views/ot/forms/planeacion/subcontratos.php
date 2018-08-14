9df12ddeeeebaed2a48bb386745e6d9d5f396319
YTL <yeiman_4@hotmail.com>
2018-08-14 07:30:26 -0500

    calcular subtotales de subcontratos

--- application/views/ot/forms/planeacion/subcontratos.php
+++ application/views/ot/forms/planeacion/subcontratos.php
@@ -20,19 +20,22 @@
 				<td ng-bind="sb.unidad"></td>
 
 				<td>
-          <input type="number" style="border: 1px solid #E65100; width:7ex" min="0" step=any ng-model="sb.cantidad" ng-init="sb.cantidad = strtonum(sb.cantidad)" ng-change="calcularSubtotales()" ng-readonly="!tr.editable">
-        </td>
+          			<input type="number" style="border: 1px solid #E65100; width:7ex" min="0" step=any ng-model="sb.cantidad" ng-init="sb.cantidad = strtonum(sb.cantidad)" ng-change="calcularSubtotales()" ng-readonly="!tr.editable">
+        		</td>
 				<td>
-          <input type="number" style="border: 1px solid #E65100; width:10ex" min="0" step=any ng-model="sb.duracion" ng-init="sb.duracion = strtonum(sb.duracion)"  ng-change="calcularSubtotales()" ng-readonly="!tr.editable">
-        </td>
+          			<input type="number" style="border: 1px solid #E65100; width:10ex" min="0" step=any ng-model="sb.duracion" ng-init="sb.duracion = strtonum(sb.duracion)"  ng-change="calcularSubtotales()" ng-readonly="!tr.editable">
+          		</td>
 
-        <td  style="display:none" ng-bind="sb.tarifa | currency:'$ ':0"></td>
+          		<td  style="display:none" ng-bind="sb.tarifa | currency:'$ ':0"></td>
 
-				<td style="text-align: right"> <small ng-bind="sb.subtarifa | currency:'$ ':2 "></small> <input type="text" ng-model="sb.subtarifa" style="max-width:10ex;" ng-init="sb.subtarifa = sb.subtarifa?sb.subtarifa:sb.tarifa" ng-change="sb.tarifa = sb.subtarifa"> </td>
+				<td style="text-align: right"> 
+					<small ng-bind="sb.subtarifa | currency:'$ ':2 "></small> 
+					<input type="text" ng-model="sb.subtarifa" style="max-width:10ex;" ng-init="sb.subtarifa = sb.subtarifa?sb.subtarifa:sb.tarifa" ng-change="sb.tarifa = sb.subtarifa; calcularSubtotales()"> 
+				</td>
 
 				<td style="text-align: right">
 					{{ ( sb.facturable?(sb.cantidad * sb.duracion)*sb.subtarifa:0 ) | currency:'$':0 }}
-					<button ng-show=" ( sb.fecha_agregado == '' || ot.estado_doc == 'POR EJECUTAR' || tr.editable == true ) " type="button" ng-click="unset_item(tr.subcontratos, sub, '<?= site_url() ?>')" class="btn red mini-btn2"> x </button>
+					<button ng-show=" ( sb.fecha_agregado == '' || ot.estado_doc == 'POR EJECUTAR' || tr.editable == true ) " type="button" ng-click="unset_item(tr.subcontratos, sb, '<?= site_url() ?>')" class="btn red mini-btn2"> x </button>
 				</td>
 
 				<td>
