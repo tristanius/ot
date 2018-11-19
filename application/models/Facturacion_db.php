2484345d11fe337b91a358f7570c6a1b922822e0
Yeison <tristan,herth@gmail.com>
2018-11-19 08:33:39 -0500

    Fix: Correccion menor al informe prod. 2

--- application/models/Facturacion_db.php
+++ application/models/Facturacion_db.php
@@ -103,13 +103,13 @@ class Facturacion_db extends CI_Model{
         rrd.cantidad,
         (rrd.cantidad * tr.tarifa) as valor_subtotal,
 
-        IFNULL((SELECT tarea_ot.a FROM tarea_ot WHERE tr.OT_idOT = OT.idOT ORDER BY tr.idtarea_ot ASC LIMIT 1), vg.a ) AS a,
-        IFNULL((SELECT tarea_ot.i FROM tarea_ot WHERE tr.OT_idOT = OT.idOT ORDER BY tr.idtarea_ot ASC LIMIT 1), vg.i ) AS i,
-        IFNULL((SELECT tarea_ot.u FROM tarea_ot WHERE tr.OT_idOT = OT.idOT ORDER BY tr.idtarea_ot ASC LIMIT 1), vg.u ) AS u,
+        IFNULL((SELECT tarea_ot.a FROM tarea_ot WHERE tarea_ot.OT_idOT = OT.idOT ORDER BY tarea_ot.idtarea_ot ASC LIMIT 1), vg.a ) AS a,
+        IFNULL((SELECT tarea_ot.i FROM tarea_ot WHERE tarea_ot.OT_idOT = OT.idOT ORDER BY tarea_ot.idtarea_ot ASC LIMIT 1), vg.i ) AS i,
+        IFNULL((SELECT tarea_ot.u FROM tarea_ot WHERE tarea_ot.OT_idOT = OT.idOT ORDER BY tarea_ot.idtarea_ot ASC LIMIT 1), vg.u ) AS u,
 
-        IFNULL((SELECT tarea_ot.a FROM tarea_ot WHERE tr.OT_idOT = OT.idOT ORDER BY tr.idtarea_ot ASC LIMIT 1), vg.a )*(rrd.cantidad * tr.tarifa) AS subtotal_a,
-        IFNULL((SELECT tarea_ot.i FROM tarea_ot WHERE tr.OT_idOT = OT.idOT ORDER BY tr.idtarea_ot ASC LIMIT 1), vg.i )*(rrd.cantidad * tr.tarifa) AS subtotal_i,
-        IFNULL((SELECT tarea_ot.u FROM tarea_ot WHERE tr.OT_idOT = OT.idOT ORDER BY tr.idtarea_ot ASC LIMIT 1), vg.u )*(rrd.cantidad * tr.tarifa) AS subtotal_u,
+        IFNULL((SELECT tarea_ot.a FROM tarea_ot WHERE tarea_ot.OT_idOT = OT.idOT ORDER BY tarea_ot.idtarea_ot ASC LIMIT 1), vg.a )*(rrd.cantidad * tr.tarifa) AS subtotal_a,
+        IFNULL((SELECT tarea_ot.i FROM tarea_ot WHERE tarea_ot.OT_idOT = OT.idOT ORDER BY tarea_ot.idtarea_ot ASC LIMIT 1), vg.i )*(rrd.cantidad * tr.tarifa) AS subtotal_i,
+        IFNULL((SELECT tarea_ot.u FROM tarea_ot WHERE tarea_ot.OT_idOT = OT.idOT ORDER BY tarea_ot.idtarea_ot ASC LIMIT 1), vg.u )*(rrd.cantidad * tr.tarifa) AS subtotal_u,
 
         p.identificacion as cedula,
         p.nombre_completo,
