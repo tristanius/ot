c60978d5fcc58ffe10800bbf8d4174796d15271e
Yeison <tristan,herth@gmail.com>
2018-11-19 09:40:32 -0500

    Fix: Mejora informe consolidado por OT 3.1

--- application/models/Facturacion_db.php
+++ application/models/Facturacion_db.php
@@ -411,7 +411,7 @@ class Facturacion_db extends CI_Model{
       ->join('tarea_ot AS tr','tr.OT_idOT = OT.idOT')
       ->join('base AS b','b.idbase = OT.base_idbase')
       ->join('especialidad AS esp','esp.idespecialidad = OT.especialidad_idespecialidad')
-      ->join('tipo_ot AS tp', 'tp.tipo_ot_idtipo_ot = OT.idtipo_ot')
+      ->join('tipo_ot AS tp', 'tp.idtipo_ot = OT.tipo_ot_idtipo_ot')
       ->join('vigencia_tarifas AS vg','vg.idvigencia_tarifas = tr.idvigencia_tarifas',"LEFT")
       ->order_by('tr.idtarea_ot','DESC')
       ->group_by('IFNULL(regot.idcosto_mes_ot, OT.idOT)')->get();
