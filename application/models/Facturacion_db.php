8f71378c43eaadd77ac09459f0c8c847b5cd5b8e
Yeison <tristan,herth@gmail.com>
2018-12-04 17:39:58 -0500

    Fix 3.1: Inf. de produccion

--- application/models/Facturacion_db.php
+++ application/models/Facturacion_db.php
@@ -164,7 +164,6 @@ class Facturacion_db extends CI_Model{
         IFNULL((SELECT tarea_ot.i FROM tarea_ot WHERE tarea_ot.OT_idOT = OT.idOT ORDER BY tarea_ot.idtarea_ot ASC LIMIT 1), vg.i )*(rrd.cantidad * tr.tarifa) AS subtotal_i,
         IFNULL((SELECT tarea_ot.u FROM tarea_ot WHERE tarea_ot.OT_idOT = OT.idOT ORDER BY tarea_ot.idtarea_ot ASC LIMIT 1), vg.u )*(rrd.cantidad * tr.tarifa) AS subtotal_u,
 
-        rrd.cc AS cc_reportado,
         p.identificacion as cedula,
         p.nombre_completo,
         e.referencia as placa_equipo,
